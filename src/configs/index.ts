9c421e6cfec0322583b310e70e82ae60476bda5a
Beatriz Salles <beatrizsallesss@gmail.com>
2023-09-19 20:15:49 -0300

    :bug: Fix: Change prod url

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,7 +3,7 @@ import { LocationOriginEnum } from "@/types";
 const {VITE_BASE_API_URL_DEV, VITE_BASE_API_URL, VITE_FREECURRENCY_KEY, VITE_CURRENCY_URL} = import.meta.env
 
 const nameFlag: { [k: string]: LocationOriginEnum } = {
-  'ewallet': LocationOriginEnum.PROD,
+  'ewallet-front': LocationOriginEnum.PROD,
   localhost: LocationOriginEnum.DEV,
 };
 
@@ -17,6 +17,7 @@ const isProdAmbient = LocationOriginEnum.PROD === ORIGIN_NAME
 export const BASE_URL_API = isProdAmbient
   ? VITE_BASE_API_URL_DEV
   : VITE_BASE_API_URL;
+
 
 export const FREECURRENCY_KEY = VITE_FREECURRENCY_KEY
 export const CURRENCY_URL = VITE_CURRENCY_URL
